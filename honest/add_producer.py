9f8164892cbd80570bb1ff2f54c0430c5a9561b1
litepresence <finitestate@tutamail.com>
2020-01-20 20:20:15 -0500

    Update add_producer.py

--- honest/add_producer.py
+++ honest/add_producer.py
@@ -23,8 +23,15 @@ from utilities import race_read_json
 # ######################################################################################
 # USER CONTROL PANEL
 # ######################################################################################
-PRODUCER_ID = ""
-ASSET_ID = ""
+
+PRODUCER_IDS = [
+    "1.2.581357",  # user85                    @litepresence.com
+    "1.2.420263",  # gabrielv0                 @Don_Gabriel
+    "1.2.1060824",  # bitshares-notifications  @murda_ra
+    "1.2.1691170",  # bitshares-robot          @AmmarYousef
+    "1.2.20638",  # datasecuritynode           @JBahai
+]
+ASSET_ID = "1.3.5641"  # HONEST.CNY
 # ######################################################################################
 # FIXME: add method to remove a producer
 # FIXME: use ASSET_NAME instead ASSET_ID; add call in pricefeed_publish.py to translate
@@ -37,13 +44,13 @@ def add_producer(name, wif):
     edicts = []
     nodes = race_read_json("nodes.txt")
     header = {
-    "account_name": name,
-    "wif": wif,
+        "account_name": name,
+        "wif": wif,
     }
     edict = {
         "op": "producer",
         "asset_id": ASSET_ID,
-        "producer_id": PRODUCER_ID,
+        "producer_ids": PRODUCER_IDS,
     }
     order = {
         "header": header,
@@ -53,6 +60,7 @@ def add_producer(name, wif):
 
     broker(order)
 
+
 def main():
     """
     input name and wif then add a new pricefeed producer
@@ -61,7 +69,7 @@ def main():
     wif = getpass("\n\nBitshares DEX wif:\n\n")
     add_producer(name, wif)
 
+
 if __name__ == "__main__":
 
     main()
-
